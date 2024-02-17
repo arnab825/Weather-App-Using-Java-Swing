2660b914bf79509d002bcb570b3815b79b801a42
Arnab Roy <rarnab225@gmail.com>
2024-02-18 00:43:09 +0530

    Hello

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import javax.swing.SwingUtilities;
+import GUI.WeatherAppGui;
+public class App {
+    public static void main(String[] args) throws Exception {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run(){
+                //deisplay our weather app gui
+                new WeatherAppGui().setVisible(true);
+                //testing: location geographical data form the API
+                //System.out.println(WeatherApp.getCurrentTime());
+               
+                
+            }
+        });
+    }
+}
